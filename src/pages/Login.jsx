5381050df225c7f634b0a7c27f15f3153ecd4c3f
Asma Aslam <156405245+asma-aslam30@users.noreply.github.com>
2025-04-27 01:01:37 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     setErrorMessage('')
 
     try {
-      const response = await axios.post('lms-backend-three-nu.vercel.app/api/users/login', {
+      const response = await axios.post('https://lms-backend-production-939d.up.railway.app//api/users/login', {
         email: formData.email,
         password: formData.password
       }, {
